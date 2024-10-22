217758619d6239979a70375863fdba5782c31530
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-10-22 16:06:08 +0200

    [fix] ciclo if

--- script/main.js
+++ script/main.js
@@ -10,32 +10,44 @@ const onOffButton = document.getElementById('on-off')
 
 
 
+// mi creo una variabile che indica lo stato della lampadina
 
+// Inizializzazione
+// se lo stato è true(Accesa)
+// Il messaggio del bottone è spegni
+// L'immagine è della lampadina accesa
 
 
 
   
 onOffButton.addEventListener('click', function () {
-    onOffButton.classList.toggle('active');
-    elementLampadina.classList.remove('lamp-on')
-    elementLampadina.classList.add('lamp-off')
-    onOffButton.innerText = 'ACCENDI'
-    elementLampadina.src='./img/white_lamp.png'
-    elementBody.style.backgroundColor='rgb(5, 28, 90)'
-    elementTitle.style.color='white'
-
+    onOffButton.classList.toggle('active')
+    let buttonText = 'ACCENDI'
+    let imgContent = './img/white_lamp.png'
+    let background = 'rgb(5, 28, 90)'
+    let color = 'white'
 
 
     if (onOffButton.classList.contains('active')){
-        onOffButton.innerText = 'SPEGNI'
-        elementLampadina.src='./img/yellow_lamp.png'
+
         elementLampadina.classList.remove('lamp-off')
         elementLampadina.classList.add('lamp-on')
-        elementBody.style.backgroundColor='rgb(88, 183, 228)'
-        elementTitle.style.color='black'
+        buttonText = 'SPEGNI'
+        imgContent ='./img/yellow_lamp.png'
+        background='rgb(88, 183, 228)'
+        color ='black'
 
 
+    }else{
+        elementLampadina.classList.remove('lamp-on')
+        elementLampadina.classList.add('lamp-off')
+        
     }
+
+    onOffButton.innerText = buttonText;
+    elementLampadina.src = imgContent
+    elementBody.style.backgroundColor = background
+    elementTitle.style.color = color
     
   
 
